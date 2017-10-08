9ee6620d4c7523a3ee3db06b985dea2611673e17
albert Byverdu :) <byverdu@gmail.com>
2017-10-08 23:24:33 +0100

    added more tests for utils methods

--- src/prettyLetters.js
+++ src/prettyLetters.js
@@ -6,7 +6,16 @@
 var utils = {
   emptySelectorError: 'EmptySelectorError, prettyLetters was called without any CSS selector',
   wrongSelectorError: 'WrongSelectorError, prettyLetters was called with a mismatched CSS selector',
-  hasWhiteSpace: function() {}
+  hasWhiteSpace: function( sentence ) {
+    return sentence.indexOf( ' ' ) > -1;
+  },
+  splitText: function( word ) {
+    return word.split( '' );
+  },
+  splitTextWhiteSpace: function( sentence ) {
+    return sentence.split( ' ' )
+    .map( function ( word ) { return word.split( '' );});
+  }
 };
 
 function prettyLetters( selector ) {
@@ -43,6 +52,24 @@ describe( 'Utils', function() {
   it( 'has a hasWhiteSpace property', function() {
     expect( utils ).to.have.property( 'hasWhiteSpace' )
       .and.is.a( 'Function' );
+  });
+  it( 'hasWhiteSpace, checks if a string has white space', function() {
+    expect( utils.hasWhiteSpace( 'single' )).to.eq( false );
+    expect( utils.hasWhiteSpace( 'single single' )).to.eq( true );
+  });
+  it( 'has a splitText property', function() {
+    expect( utils ).to.have.property( 'splitText' )
+      .and.is.a( 'Function' );
+  });
+  it( 'splitText, split a word into letters', function() {
+    expect( utils.splitText( 'single' )).to.eql(['s', 'i', 'n', 'g', 'l', 'e']);
+  });
+  it( 'has a splitTextWhiteSpace property', function() {
+    expect( utils ).to.have.property( 'splitTextWhiteSpace' )
+      .and.is.a( 'Function' );
+  });
+  it( 'splitTextWhiteSpace, split a word into letters', function() {
+    expect( utils.splitTextWhiteSpace( 'single single' )).to.eql([['s', 'i', 'n', 'g', 'l', 'e'], ['s', 'i', 'n', 'g', 'l', 'e']]);
   });
 });
 /* end-dev-code */
